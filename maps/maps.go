f05217a1e72143fdd1008ba4b67a8d0bb126836b
adu <aluta182004@gmail.com>
2024-07-03 07:27:06 +0000

    tdd with maps

---
+++ maps/maps.go
@@ -0,0 +1,56 @@
+package maps
+
+type Dictionary map[string]string
+type DictionaryErr string
+
+const (
+	ErrorNotFound       = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Search(searchterm string) (string, error) {
+	definition, ok := d[searchterm]
+
+	if !ok {
+		return "", ErrorNotFound
+	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrorNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrorNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
